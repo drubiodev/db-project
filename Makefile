3e87ae2607b3567d5f5e3f9a64fd42c645331741
Daniel Rubio <drubio432@gmail.com>
2025-01-25 23:13:46 -0500

    initial commit

---
+++ Makefile
@@ -0,0 +1,24 @@
+TARGET = bin/dbview
+SRC = $(wildcard src/*.c)
+OBJ = $(patsubst src/%.c, obj/%.o, $(SRC))
+
+run: folders clean default
+	./$(TARGET)
+	./$(TARGET) -f mydb.db
+
+default: $(TARGET)
+
+folders:
+	mkdir -p bin
+	mkdir -p obj
+
+clean: 
+	rm -f obj/*.o
+	rm -f bin/*
+	rm -f *.db
+
+$(TARGET): $(OBJ)
+	gcc -o $@ $?
+
+obj/%.o: src/%.c
+	gcc -c $< -o $@ -Iinclude
---
+++ src/main.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <getopt.h>
+
+// #include "common.h"
+// #include "file.h"
+// #include "parse.h"
+
+void print_usage(char *argv[])
+{
+  printf("Usage: %s -f <filepath> [-n] [-p <port>] [-l]\n", argv[0]);
+  printf("Options:\n");
+  printf("  -f <filepath>  Path to the file to be served\n");
+  printf("  -n             Create a new file\n");
+  printf("  -p <port>      Port to listen on\n");
+  printf("  -l             List files in the directory\n");
+
+  return;
+}
+
+int main(int argc, char *argv[])
+{
+  char *filepath = NULL;
+  char *portarg = NULL;
+  unsigned short port = 0;
+  bool newfile = false;
+  bool list = false;
+  int c;
+
+  while ((c = getopt(argc, argv, "nf:a:l")) != -1)
+  {
+    switch (c)
+    {
+    case 'n':
+      newfile = true;
+      break;
+    case 'f':
+      filepath = optarg;
+      break;
+    case 'p':
+      portarg = optarg;
+      break;
+    case 'l':
+      list = true;
+      break;
+    case '?':
+      printf("Unknown option -%c\n", c);
+      break;
+    default:
+      return -1;
+    }
+  }
+
+  // require filepath
+  if (filepath == NULL)
+  {
+    printf("Filepath is required argument\n");
+    print_usage(argv);
+    return -1;
+  }
+}

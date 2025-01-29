da5a6f5c87d58ef8b0dd76fa4ee88a4552147262
Daniel Rubio <drubio432@gmail.com>
2025-01-28 21:36:13 -0500

    add to open file method

--- src/file.c
+++ src/file.c
@@ -27,3 +27,16 @@ int create_db_file(char *filename)
 
   return fd;
 }
+
+int open_db_file(char *filename)
+{
+  int fd = open(filename, O_RDWR | O_CREAT, 0644);
+
+  if (fd == -1)
+  {
+    perror("open");
+    return STATUS_ERROR;
+  }
+
+  return fd;
+}
